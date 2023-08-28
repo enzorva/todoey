5b91a932e88973083f99d16d8d0fde38ca0e560b
Enzo <enzo.rva@gmail.com>
2023-08-28 18:14:40 -0300

    screen add task created

---
+++ lib/screens/add_tasks_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    late String newTaskTitle;
+
+    return Container(
+      color: const Color(0xff757575),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              onChanged: (newText) {
+                newTaskTitle = newText;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(newTaskTitle);
+              },
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
