5077a495e1edda20a77ca4b60c8e6643aa8f9ee7
Enzo <enzo.rva@gmail.com>
2023-08-28 18:14:03 -0300

    task screen created

---
+++ lib/widgets/tasks_list.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/widgets/tasks_tile.dart';
+import 'package:todoey/models/task.dart';
+
+class TasksList extends StatefulWidget {
+  final List<Task> tasks;
+
+  TasksList(this.tasks);
+
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (contex, index) {
+        return TaskTile(
+            taskTitle: widget.tasks[index].name,
+            isChecked: widget.tasks[index].isDone,
+            checkboxCallback: (checkboxState) {
+              setState(() {
+                widget.tasks[index].toggleDone();
+              });
+            });
+      },
+      itemCount: widget.tasks.length,
+    );
+  }
+}
