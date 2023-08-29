6647916bf9a3fece1ab16139aeeb88b0fdbf56cb
Enzo <enzo.rva@gmail.com>
2023-08-29 18:34:49 -0300

    list moved to new file and func created

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,31 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/widgets/tasks_tile.dart';
-import 'package:todoey/models/task.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task_data.dart';
 
-class TasksList extends StatefulWidget {
-  final List<Task> tasks;
-
-  TasksList(this.tasks);
-
-  @override
-  State<TasksList> createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> {
+class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (contex, index) {
-        return TaskTile(
-            taskTitle: widget.tasks[index].name,
-            isChecked: widget.tasks[index].isDone,
-            checkboxCallback: (checkboxState) {
-              setState(() {
-                widget.tasks[index].toggleDone();
-              });
-            });
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (contex, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+                taskTitle: task.name,
+                isChecked: task.isDone,
+                longPressCallback: () {
+                  taskData.deleteTask(task);
+                },
+                checkboxCallback: (checkboxState) {
+                  taskData.updateTask(task);
+                });
+          },
+          itemCount: taskData.taskCount,
+        );
       },
-      itemCount: widget.tasks.length,
     );
   }
 }
